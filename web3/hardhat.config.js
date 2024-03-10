54df7faff61ccd462143faae82a6bfc26670a076
jjjutla <jeevan.jutla@gmail.com>
2024-03-10 11:10:40 +0000

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -5,7 +5,7 @@ const config = {
   networks: {
     etherlinkTest: {
       url: "https://node.ghostnet.etherlink.com",
-      accounts: "4d3f0c7c7e200fac06c872a18eb1e9bd195c7e0682d29ca4bda9055aed05bf2d",
+      accounts: "[REDACTED]",
     }
   }
 };
